b4598251607b9836ff6d8721bfca20f114a50246
franklin-junior1 <franklin.junior@vodafone.com>
2024-09-03 20:45:56 +0100

    "Refactored AuthRemoteDataSourceTest: added new imports, classes, and variables; modified setUp and test methods; removed old code"

--- test/src/auth/data/data_source/remote/auth_remote_data_source_test.dart
+++ test/src/auth/data/data_source/remote/auth_remote_data_source_test.dart
@@ -1,91 +1,121 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:education_project/core/errors/exceptions.dart';
+import 'package:education_project/core/utils/typedef.dart';
 import 'package:education_project/src/auth/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:education_project/src/auth/data/model/user_model.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+import 'package:mocktail/mocktail.dart';
+
+const tFullname = 'Franklin test';
+const tEmail = 'n0p6b@example.com';
+const tPassword = '123456';
+final tFirebaseAuthException = FirebaseAuthException(
+  code: 'user-not-found',
+  message: 'There is no user record corresponding to this identifier.',
+);
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {
+  /*  User? _user;
+  @override
+  User? get currentUser => null;
+
+  set currentUser(User? user) {
+    if (_user != user) {
+      _user = user;
+    }
+  } */
+}
+
+class MockUser extends Mock implements User {
+  String _uid = '!@3456';
+
+  @override
+  String get uid => _uid;
+
+  set uid(String value) {
+    _uid = value;
+  }
+}
+
+class MockUserCredential extends Mock implements UserCredential {
+  MockUserCredential([User? user]) : _user = user;
+  User? _user;
+
+  @override
+  User? get user => _user;
+
+  set user(User? value) {
+    if (_user != value) {
+      _user = value;
+    }
+  }
+}
 
 void main() {
-  late FakeFirebaseFirestore cloudStoreClient;
-  late MockFirebaseAuth mockFirebaseAuth;
-  late MockFirebaseStorage mockFirebaseFirestore;
+  late FirebaseAuth authClient;
+  late FirebaseStorage storageClient;
+  late FirebaseFirestore firestoreClient;
   late AuthRemoteDataSource authRemoteDataSource;
+  late UserCredential userCredential;
+  late DocumentReference<DataMap> documentReference;
+  late MockUser mockUser;
 
-  const tPassword = 'Password';
-  const tEmail = 'QpNQ8@example.com';
-  const tFullName = 'John Doe';
+  final tUser = LocalUserModel.empty();
 
-  setUp(() async {
-    cloudStoreClient = FakeFirebaseFirestore();
-    final googleSignIn = MockGoogleSignIn();
-    final signInAccount = await googleSignIn.signIn();
-    final googleAuth = await signInAccount?.authentication;
-    final authCredential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
+  setUpAll(() async {
+    authClient = MockFirebaseAuth();
+    storageClient = MockFirebaseStorage();
+    firestoreClient = FakeFirebaseFirestore();
+    documentReference = await firestoreClient.collection('Users').add(
+          tUser.toMap(),
+        );
+    mockUser = MockUser()..uid = documentReference.id;
 
-    // Mock User
-    final mockUser = MockUser(
-      uid: '123',
-      displayName: 'John Doe',
-      email: 'QpNQ8@example.com',
-      photoURL: 'https://example.com/profile.jpg',
-    );
-    mockFirebaseAuth = MockFirebaseAuth(mockUser: mockUser);
-    final authResult =
-        await mockFirebaseAuth.signInWithCredential(authCredential);
-    final user = authResult.user;
-    mockFirebaseFirestore = MockFirebaseStorage();
-
+    userCredential = MockUserCredential(mockUser);
     authRemoteDataSource = AuthRemoteDataSourceImpl(
-      firebaseAuth: mockFirebaseAuth,
-      firebaseFirestore: cloudStoreClient,
-      firebaseStorage: mockFirebaseFirestore,
+      firebaseAuth: authClient,
+      firebaseFirestore: firestoreClient,
+      firebaseStorage: storageClient,
     );
+
+    when(() => authClient.currentUser).thenReturn(mockUser);
   });
 
-  test('[signUp] from remote data source', () async {
-    // arrange
+  group('forgotPassword', () {
+    test('should call forgotPassword from [AuthRepository]', () async {
+      when(
+        () => authClient.sendPasswordResetEmail(email: any(named: 'email')),
+      ).thenAnswer((_) async => Future.value);
 
-    // act
-    await authRemoteDataSource.signUp(
-      email: tEmail,
-      fullName: tFullName,
-      password: tPassword,
+      final call = authRemoteDataSource.forgotPassword(tEmail);
+      expect(call, completes);
+
+      verify(() => authClient.sendPasswordResetEmail(email: tEmail)).called(1);
+      verifyNoMoreInteractions(authClient);
+    });
+
+    test(
+      'should throw [ServerException] when [FirebaseAuthException] is throwm',
+      () async {
+        // arrange
+        when(
+          () => authClient.sendPasswordResetEmail(email: any(named: 'email')),
+        ).thenThrow(
+          tFirebaseAuthException,
+        );
+        // act
+        final call = authRemoteDataSource.forgotPassword;
+
+        // assert
+        expect(() => call(tEmail), throwsA(isA<ServerException>()));
+        verify(() => authClient.sendPasswordResetEmail(email: tEmail))
+            .called(1);
+        verifyNoMoreInteractions(authClient);
+      },
     );
-
-    //Expect that the user was created on firestore and authclient
-    final user = await cloudStoreClient
-        .collection('Users')
-        .doc(mockFirebaseAuth.currentUser?.uid)
-        .get();
-
-    // assert
-    expect(mockFirebaseAuth.currentUser, isNotNull);
-    expect(mockFirebaseAuth.currentUser?.displayName, tFullName);
-    expect(mockFirebaseAuth.currentUser?.email, tEmail);
-
-    expect(user.exists, isTrue); // user was created on firestore
-  });
-
-  test('[signIn] from remote data source', () async {
-    // arrange
-    await authRemoteDataSource.signUp(
-      email: 'tEmail@email.com',
-      fullName: tFullName,
-      password: tPassword,
-    );
-    await mockFirebaseAuth.signOut();
-
-    // act
-    await authRemoteDataSource.signIn(
-      email: 'tEmail@email.com',
-      password: tPassword,
-    );
-    // assert
-    expect(mockFirebaseAuth.currentUser, isNotNull);
-    expect(mockFirebaseAuth.currentUser?.email, 'tEmail@email.com');
   });
 }
